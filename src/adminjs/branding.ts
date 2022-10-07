a860aacbf42eda6fc929fe4f1d64accfa1664574
João Maia <15joaomaia@gmail.com>
2022-10-07 10:44:32 -0300

    Mais correções de estilos

--- src/adminjs/branding.ts
+++ src/adminjs/branding.ts
@@ -5,13 +5,13 @@ export const brandingOptions: BrandingOptions = {
   theme: {
     colors: {
       primary100: "#68D391",
-      primary80: "#68D391",
+      primary80: "#38A169",
       primary60: "#9AE6B4",
       primary40: "#C6F6D5",
       primary20: "#F0FFF4",
       grey100: "#151515",
       grey80: "#333333",
-      grey60: "#ffffff",
+      grey60: "#111",
       grey40: "#666666",
       grey20: "#68D391",
       filterBg: "#333333",
