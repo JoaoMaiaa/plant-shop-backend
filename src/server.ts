ac2d49cec98f02e0511e3e4162f1361005c66d6d
João Maia <15joaomaia@gmail.com>
2022-10-07 17:51:39 -0300

    Iniciando os controllers e as rotas

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,17 @@ import express from "express";
 
 import { sequelize } from "./database";
 import { adminJs, adminJsRouter } from "./adminjs";
+import { router } from "./routes";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use(adminJs.options.rootPath, adminJsRouter);
+
+app.use(router);
 
 app.listen(PORT, () => {
   sequelize.authenticate().then(() => {
