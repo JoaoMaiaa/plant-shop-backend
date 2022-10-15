cffa46c3c97c7f9ca861fe5d67942c5fe6380f12
João Maia <15joaomaia@gmail.com>
2022-10-15 11:44:45 -0300

    Adicionada a tela de login com a autenticação

--- src/adminjs/index.ts
+++ src/adminjs/index.ts
@@ -1,12 +1,17 @@
 import AdminJS from "adminjs";
 import AdminJSExpress from "@adminjs/express";
 import AdminJSSequelize from "@adminjs/sequelize";
+import bcrypt from "bcrypt";
+const dotenv = require("dotenv");
+dotenv.config();
 
 import { sequelize } from "../database";
 import { adminJsResource } from "./resources";
 import { locale } from "./locale";
 import { dashboardOptions } from "./dashboard";
 import { brandingOptions } from "./branding";
+// import { Consumer } from "../models/Consumer";
+import { Consumer, ConsumerInstance } from "../models/Consumer";
 
 AdminJS.registerAdapter(AdminJSSequelize);
 
@@ -19,4 +24,28 @@ export const adminJs = new AdminJS({
   branding: brandingOptions,
 });
 
-export const adminJsRouter = AdminJSExpress.buildRouter(adminJs);
+export const adminJsRouter = AdminJSExpress.buildAuthenticatedRouter(
+  adminJs,
+  {
+    authenticate: async (email, password) => {
+      const consumer = await Consumer.findOne({
+        where: { email },
+      });
+
+      if (consumer) {
+        const matched = await bcrypt.compare(password, consumer.password);
+
+        if (matched) {
+          return consumer;
+        }
+      }
+      return false;
+    },
+    cookiePassword: `${process.env.COOKIE}`,
+  },
+  null,
+  {
+    resave: false,
+    saveUninitialized: false,
+  }
+);
