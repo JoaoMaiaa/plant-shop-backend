417e6b7f93d531c68530ac6827123e5666d05d03
João Maia <15joaomaia@gmail.com>
2022-10-15 22:19:07 -0300

    Correções gerais

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 // import express from "express";
-const express = require('express')
+const express = require("express");
 
 // import { sequelize } from "./database";
-const sequelize = require('./database')
-import { adminJs, adminJsRouter } from "./adminjs";
-import { router } from "./routes";
+const sequelize = require("./database");
+// import { adminJs, adminJsRouter } from "./adminjs";
+const { adminjs, adminJsRouter } = require("./adminjs");
+// import { router } from "./routes";
+const router = require("./routes");
 
 const app = express();
 
