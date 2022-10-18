971e872503a173443c99b9041506a4110161aa8a
João Maia <15joaomaia@gmail.com>
2022-10-18 10:22:24 -0300

    Adicionando o dotenv no server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 dotenv.config();
 
 import { sequelize } from "./database";
